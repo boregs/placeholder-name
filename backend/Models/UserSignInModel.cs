c24b7486dbdd9df3d53324e7a1edf806a9a3f83b
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-02-05 13:30:28 -0300

    Create UserSignInModel.cs

---
+++ backend/Models/UserSignInModel.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace WebsiteAccessControl.Models
+{
+  public class UserSignIn(){
+    
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public DateOnly dateOfBirth { get; set; }
+
+        public string username { get; set; }
+
+        public string email { get; set; }
+    
+        public string password { get; set; }
+
+        public string nationality { get; set; }
+  }
+}
